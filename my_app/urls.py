1610cadf16237de1cc79d50f377e85527ca2ba71
Murodjon Tokhirov <murodtokh1997@gmail.com>
2023-05-28 12:46:47 +0900

    Developing authentiocation for our project

--- my_app/urls.py
+++ my_app/urls.py
@@ -6,4 +6,7 @@ app_name = 'my_app'
 urlpatterns = [
     path('about/', views.about, name='about'),
     path('index/', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login, logout as auth_logout
+from django.contrib.auth.decorators import login_required
 from .models import Iqtibos
-
 # Create your views here.
 
 def index(request):
@@ -10,3 +12,28 @@ def index(request):
 
 def about(request):
     return render(request,'my_app/about.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('my_app:login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'auth_templates/register.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('my_app:index')  # Replace 'home' with your desired URL after login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'auth_templates/login.html', {'form': form})
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect('my_app:login')
