1e371b2e7e1c90efdecf72d401f9add40a0e5279
Murodjon Tokhirov <murodtokh1997@gmail.com>
2023-06-07 19:13:34 +0900

    adding bank accounts

--- my_app/models.py
+++ my_app/models.py
@@ -9,4 +9,12 @@ class Iqtibos(models.Model):
 
     def __str__(self):
         return self.title
+
+class BankAccount(models.Model):
+    name = models.CharField(max_length=100)
+    account_number = models.IntegerField(null=True,blank=True)
+
+    def __str__(self):
+        return self.name
     
+
