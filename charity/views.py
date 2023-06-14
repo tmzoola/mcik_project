439cfb3a1a0d7881f0227f0a2a356033efb3ca9d
Murodjon Tokhirov <murodtokh1997@gmail.com>
2023-06-14 22:34:24 +0900

    views.py for paypal and temporary home template

--- charity/views.py
+++ charity/views.py
@@ -1,19 +1,24 @@
+import uuid
 from django.shortcuts import render
 from paypal.standard.forms import PayPalPaymentsForm
 from django.conf import settings
+from django.urls import reverse
 # Create your views here.
 
 def home(request):
-        
+    host = request.get_host()
+    
+
     paypal_dict = {
         "business": settings.PAYPAL_RECEIVER_EMAIL,
-        "amount": "10000000.00",
-        "item_name": "name of the item",
-        "invoice": "unique-invoice-id",
-        "notify_url": request.build_absolute_uri(reverse('paypal-ipn')),
-        "return": request.build_absolute_uri(reverse('your-return-view')),
-        "cancel_return": request.build_absolute_uri(reverse('your-cancel-view')),
-        "custom": "premium_plan",  # Custom command to correlate to some function later (optional)
+        "amount": "20.00",
+        "item_name": "donation 1",
+        "invoice": str(uuid.uuid4()),
+        "notify_url": f'http://{host}{reverse("paypal-ipn")}',
+        "return":f'http://{host}{reverse("paypal-reverse")}' ,
+        "cancel_return": f'http://{host}{reverse("paypal-cancel")}',
     }
     form = PayPalPaymentsForm(initial=paypal_dict)
-    context = {"form": form}    
+    context = {"form": form}
+
+    return render(request,'charity/home.html',context)    
